4a0783804e77dd6c3802185bec0e7a2139dadc64
SungJaeYu <ysj7045@naver.com>
2020-11-02 21:29:40 +0900

    Completed my CSS design & deploy my website on github

--- src/Movie.js
+++ src/Movie.js
@@ -6,10 +6,12 @@ function Movie({ id, year, title, summary, poster, genres }){
     return <div className="movie">
         <img src={poster} alt={title} title={title}/>
         <div className="movie__data">
-        <h3 className="movie__title">{title}</h3>
-        <h5 className="movie__year">{year}</h5>
-        <p className="movie__summary">{summary}</p>
-        <ul className="genres">{genres.map((genre, index) => 
+        <div className="movie__header">
+            <h3 className="movie__title">{title}</h3>
+            <h5 className="movie__year">{year}</h5>
+        </div>
+        <p className="movie__summary">{summary.slice(0, 140)}...</p>
+        <ul className="movie__genres">{genres.map((genre, index) => 
         <li key={index} className="genres__genre">
             {genre}
         </li>)}
